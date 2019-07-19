812ff243bef88182e40c99737a65cb3516fcd026
Girls Who Code <GWC@cs81-gwc-05.local>
2019-07-19 13:57:09 -0400

    to do list app

--- ToDoListApp/ViewController.swift
+++ ToDoListApp/ViewController.swift
@@ -7,9 +7,27 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var inputTaskTextField: UITextField!
+    @IBAction func handleAddTaskButtonPress(_ sender: Any) {
+        addNewTask()
+    }
+    func addNewTask(){
+        if(inputTaskTextField.text != ""){
+            let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+            let newTask = Task(context:context) 
+            newTask.taskName = inputTaskTextField?.text
+            (UIApplication.shared.delegate as! AppDelegate).saveContext()
+            print(newTask.taskName!)  //for testing purposes only
+            inputTaskTextField.text = ""
+            inputTaskTextField.resignFirstResponder()
+            
+        }
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
